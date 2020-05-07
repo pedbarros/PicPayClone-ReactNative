0548d02244796aab43425bc0a7b563183687db7d
Pedro Barros <pedro.barros@herospark.com>
2020-05-07 20:03:56 -0300

    criando componentes de banner e tipos

--- src/components/Tips/index.js
+++ src/components/Tips/index.js
@@ -3,15 +3,53 @@ import React from 'react';
 import { Container, Options, Title, Img } from './styles';
 
 import img8 from '../../images/08.png';
+import img9 from '../../images/09.png';
+import img10 from '../../images/10.png';
+import img11 from '../../images/11.png';
+import img12 from '../../images/12.png';
 
-export default function Tips() {
+const items = [
+  {
+    key: String(Math.random()),
+    img: img8,
+    title: 'Pague suas contas sem sair de casa',
+    bgColor: '#172c4a'
+  },
+  {
+    key: String(Math.random()),
+    img: img9,
+    title: 'Pague suas contas sem sair de casa',
+    bgColor: '#6a0159'
+  },
+  {
+    key: String(Math.random()),
+    img: img10,
+    title: 'Pague suas contas sem sair de casa',
+    bgColor: '#4139c8'
+  },
+  {
+    key: String(Math.random()),
+    img: img11,
+    title: 'Pague suas contas sem sair de casa',
+    bgColor: '#00ab4b'
+  },
+  {
+    key: String(Math.random()),
+    img: img12,
+    title: 'Pague suas contas sem sair de casa',
+    bgColor: '#ba2f76'
+  }
+];
+
+export default function Tips () {
   return (
     <Container>
-      <Options>
-        <Title>Pague suas contas sem sair de casa</Title>
-
-        <Img source={img8} />
-      </Options>
+      {items.map((item) => (
+        <Options key={item.key} bgColor={item.bgColor}>
+          <Title>{item.title}</Title>
+          <Img source={item.img} />
+        </Options>
+      ))}
     </Container>
   );
 }
--- src/components/Tips/styles.js
+++ src/components/Tips/styles.js
@@ -4,13 +4,28 @@ export const Container = styled.ScrollView.attrs(() => ({
   horizontal: true,
   showHorizontalScrollIndicator: false,
   contentContainerStyle: {
-    alignItems: 'center',
     paddingLeft: 16,
   }
 }))`
   margin-top: 25px;
 `
 
-export const Options = styled.TouchableOpacity``;
-export const Title = styled.Text``;
-export const Img = styled.Image``;
+export const Options = styled.TouchableOpacity`
+  background: ${({ bgColor }) => bgColor};
+  width: 150px;
+  height: 230px;
+  border-radius: 8px;
+  padding: 15px;
+  justify-content: space-between;
+  margin-right: 16px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Img = styled.Image`
+  align-self: center;
+`;
